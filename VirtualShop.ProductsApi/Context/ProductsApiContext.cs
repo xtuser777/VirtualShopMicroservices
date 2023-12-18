a46933dad86f9149aeaae4df19e47b614adadf53
LUCAS S. OLIVEIRA <lucaoxt@gmail.com>
2023-12-18 13:43:18 -0300

    ProductsApi microservice

---
+++ VirtualShop.ProductsApi/Context/ProductsApiContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using VirtualShop.ProductsApi.Models;
+
+namespace VirtualShop.ProductsApi.Context;
+
+public class ProductsApiContext : DbContext
+{
+    public ProductsApiContext(DbContextOptions<ProductsApiContext> options) : base(options)
+    {
+    }
+
+    public DbSet<Category> Categories { get; set; }
+    public DbSet<Product> Products { get; set; }
+}
---
+++ VirtualShop.ProductsApi/Models/Product.cs
@@ -0,0 +1,14 @@
+﻿namespace VirtualShop.ProductsApi.Models;
+
+public class Product
+{
+    public int Id { get; set; }
+    public string? Name { get; set; }
+    public string? Description { get; set; }
+    public decimal? Price { get; set; }
+    public long Stock { get; set; }
+    public string? ImageUrl { get; set; }
+
+    public int CategoryId { get; set; }
+    public Category? Categpry { get; set; }
+}
